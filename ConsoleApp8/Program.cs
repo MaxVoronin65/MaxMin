5d8f7f3287715bfa2f9d3a29713f968a86b4e256
MaxVoronin65 <84362737+MaxVoronin65@users.noreply.github.com>
2021-05-18 10:03:47 +0700

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пользователь вводит данные
+            Console.WriteLine("Формула для которой подбираем значения: Z = max(A + 1, min(B, C + D))      |");
+            Console.WriteLine("___________________________________________________________________________|");
+            Console.WriteLine("~");
+            Console.WriteLine("Найдем значение min, для этого введем значения (Для продолжения нажми Enter)");
+            Console.ReadKey();
+            Console.WriteLine("Введи значение для B");
+            double B = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введи значение для C");
+            double C = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введи значение для D");
+            double D = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Теперь введем значение A");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Все значения введены, для рассчета формула нажмите Enter");
+            Console.ReadKey();
+
+            //Метод нахождения решения данной задачи
+            double Z;
+            double Max()
+            {
+                double Min = 0;
+                double G = C + D;
+                double A1 = A + 1;
+                if (B < G)
+                {
+                    Min = B;
+                }
+                else
+                {
+                    Min = G;
+                }
+                if (A1 > Min)
+                {
+                    Z = A1;
+                }
+                else
+                {
+                    Z = Min;
+                }
+                return Z;
+            }
+
+            //Вывод ответа на экран
+            double sum = Max();
+            Console.WriteLine("В формуле max(A + 1, min(B, C + D)) Z = {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
